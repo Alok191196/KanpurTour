bacd75b61fb119724ff75732e157115f09a5ea1e
Alok kumar Singh <aloksingh191196@gmail.com>
2018-04-14 15:41:34 +0530

    Add files via upload

---
+++ app/build/generated/source/buildConfig/release/divy/knpurtour/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package divy.knpurtour;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "divy.knpurtour";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
